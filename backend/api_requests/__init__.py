551ab740f14ce23c15ee0cbdf44f8d858879e5ca
nschaefer1 <nathanael.rschaefer@gmail.com>
2025-11-04 18:22:14 -0600

    - Initial backend commit

---
+++ backend/api_requests/__init__.py
@@ -0,0 +1,7 @@
+
+
+from backend.api_requests.delete import *
+from backend.api_requests.get import *
+from backend.api_requests.patch import *
+from backend.api_requests.put import *
+from backend.api_requests.post import *
---
+++ backend/api_requests/delete.py
@@ -0,0 +1,4 @@
+import logging 
+
+class APIDelete:
+    pass
