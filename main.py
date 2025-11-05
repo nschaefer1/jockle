608260f1fa4a0861d95a9128b2fbb292c007a32c
nschaefer1 <nathanael.rschaefer@gmail.com>
2025-11-04 18:25:25 -0600

    - Adding the logger starter on the app entry point

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from managers import (
     MainApp
 )
 
+from utils import (
+    init_basic_logger
+)
+
 #################################################################
 # App Entry point
 #################################################################
 
 if __name__ == '__main__':
+    init_basic_logger()
     app = MainApp()
     app.run()
