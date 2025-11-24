7296b9ec27aa94f7a9501265f14db6e90ffddd25
nschaefer1 <nathanael.rschaefer@gmail.com>
2025-11-23 21:59:19 -0600

    - Adding some path managements into the main API

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import os
 
 from .api_requests import (
     APIGet, 
@@ -18,4 +20,13 @@ class API(APIGet, APIDelete, APIPatch, APIPost, APIPut):
         self.db_manager = db_manager # DB manager instance is passed in
         self.session = {}
 
+    def resolve_path(self, rel_path: str) -> str:
+        full = self.app_path(f'../frontend/{rel_path}')
+        return full.replace('\\', '/')
     
+    def app_path(self, relative_path):
+        if getattr(sys, 'frozen', False):   # Checks to see if we're running as an executable
+            base_dir = os.path.dirname(sys.executable)
+        else:   # This is the dev-env
+            base_dir = os.path.dirname(__file__)
+        return os.path.join(base_dir, relative_path)
