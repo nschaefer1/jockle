3a7b4112ae03b84475d518c711a0132d5052ef62
nschaefer1 <nathanael.rschaefer@gmail.com>
2025-11-28 15:51:50 -0600

    Chore: adding a comment on delete API child-class reminding about "pragma fk = on;" before other methods are added

--- backend/api_requests/delete.py
+++ backend/api_requests/delete.py
@@ -6,3 +6,5 @@ class APIDelete(BaseAPI):
     
     def __init__(self, db_manager):
         self.db_manager = db_manager
+
+    # TODO add a "PRAGMA foreign_keys = ON;" before we do any deletions to ensure cascading
