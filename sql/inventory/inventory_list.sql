54c463a2d7670b2c59111f38be716556d2d6a7d5
nschaefer1 <nathanael.rschaefer@gmail.com>
2025-12-14 21:31:59 -0600

    Refactor: changing the SQL associated with the inventory list to make it more effiicent

--- sql/inventory/inventory_list.sql
+++ sql/inventory/inventory_list.sql
@@ -1,13 +1,22 @@
 
+with agg_table as (
+	select
+		inv_ck,
+		sum(val) as item_count
+	from ft_inventory 
+	where char_ck = ?
+	group by 1
+)
+
 select 
 	a.inv_ck,
-	b.inv_name,
-	b.inv_desc,
-	b.icon_path,
-	sum(a.val) as item_count
-from ft_inventory as a
-right join dim_inventory as b
+	a.inv_name,
+	a.inv_desc,
+	a.icon_path,
+	coalesce(b.item_count, 0) as item_count
+from dim_inventory as a
+left join agg_table as b
 	on a.inv_ck = b.inv_ck
-where a.char_ck = ?
-group by 1,2,3,4
 order by 2;
+
+
